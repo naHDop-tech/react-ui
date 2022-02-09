778132e2848586f59b3e2ff6f4c983f2201dd264
Grigory <tarasov198726@gmail.com>
2022-02-10 01:50:07 +0300

    feat: add aliases to jest config

--- jest.config.ts
+++ jest.config.ts
@@ -82,7 +82,8 @@ export default {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    "^applicationRoot/(.*)$": "<rootDir>/src/$1",
+    "@ui/(.*)": "<rootDir>/src/ui/$1",
+    "@components/(.*)": "<rootDir>/src/components/$1",
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
