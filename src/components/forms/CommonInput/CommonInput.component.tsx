b4924dd627bc180bc0b4027fbd790b81a9b0c556
Grigory <58467895+naHDop-tech@users.noreply.github.com>
2022-02-13 13:37:40 +0300

    feat: implement BaseInput, TextInput component (#3)
    * refactor: change readme
    
    * feat: implement base input component (#2)
    
    * feat: add CommonInput component
    
    * refactor: change COLORS variable sstructure
    
    * feat: add hasError style
    
    * feat: implement error message, label
    
    * feat: add wide prop
    
    * feat: add small text mixin
    
    * refactor: minor refactoring
    
    * feat: add style to disabled prop
    
    * feat: change package version
    
    * refactor: implement TextInput component from Base input
    
    * refactor: change readme, package.json

---
+++ src/components/forms/CommonInput/CommonInput.component.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Wrapper, BaseInput } from './styles'
+import { ICommonInputProps } from './types'
+
+export function CommonInput(props: ICommonInputProps) {
+    const { label, errorMessage, hasError } = props
+
+    return (
+        <Wrapper hasError={hasError}>
+            {label && <div className='label'>{label}</div>}
+            <BaseInput {...props} />
+            {hasError && <div className='error-message'>{errorMessage}</div>}
+        </Wrapper>
+    )
+}
